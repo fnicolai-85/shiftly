9e42679622e23f4a71569ecccd6cc07e8d2c2bc4
Filippo <filippo.nicolai85@gmail.com>
2025-04-19 18:23:44 -0400

    Working to fix parcel issues

---
+++ dist/Website.53c69205.js
@@ -0,0 +1,6 @@
+function openNav() {
+    const element = document.querySelector(".nav");
+    element.classList.toggle("collapsible--expanded");
+}
+
+//# sourceMappingURL=Website.53c69205.js.map
---
+++ dist/Website.fb85b7cf.js
@@ -0,0 +1,5 @@
+function toggleMenu(element) {
+    element.classList.toggle("open");
+}
+
+//# sourceMappingURL=Website.fb85b7cf.js.map
