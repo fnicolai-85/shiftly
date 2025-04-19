e772b342224236f8686e8fee6d5662c833367541
Filippo <filippo.nicolai85@gmail.com>
2025-04-19 18:43:55 -0400

    Deleted dist to restart process

--- dist/Website.53c69205.js
+++
@@ -1,6 +0,0 @@
-function openNav() {
-    const element = document.querySelector(".nav");
-    element.classList.toggle("collapsible--expanded");
-}
-
-//# sourceMappingURL=Website.53c69205.js.map
--- dist/Website.fb85b7cf.js
+++
@@ -1,5 +0,0 @@
-function toggleMenu(element) {
-    element.classList.toggle("open");
-}
-
-//# sourceMappingURL=Website.fb85b7cf.js.map
